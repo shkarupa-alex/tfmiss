e8c2b3f4a3cc508e7b50f6ffa396d009593cae60
Shkarupa Alex <shkarupa.alex@gmail.com>
2020-04-22 22:39:58 +0400

    Reformat code

--- tfmiss/preprocessing/preprocessing_test.py
+++ tfmiss/preprocessing/preprocessing_test.py
@@ -61,11 +61,11 @@ class ContBowTest(tf.test.TestCase):
             [1, 0, 0],
             [-1, 1, 0],
             [-1, 1, 0],
-            [-2, - 1, 0],
+            [-2, -1, 0],
             [1, 0, 0],
             [-1, 1, 2],
             [-1, 1, 0],
-            [-2, - 1, 0],
+            [-2, -1, 0],
         ], positions.tolist())
 
     def test_ragged(self):
