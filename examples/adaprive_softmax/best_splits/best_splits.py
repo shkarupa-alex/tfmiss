Fix adaptive softmax demo

examples/adaprive_softmax/best_splits/best_splits.py
@@ -32,6 +32,11 @@ if __name__ == "__main__":
         type=int,
         help='Hidden size')
     parser.add_argument(
+        '--factor',
+        type=int,
+        default=4,
+        help='Scale factor for tail projections')
+    parser.add_argument(
         '--classes_vocab',
         type=argparse.FileType('r'),
         default=None,
@@ -46,20 +51,15 @@ if __name__ == "__main__":
         default=Vocabulary.FORMAT_BINARY_PICKLE,
         help='Vocabulary file format')
     parser.add_argument(
-        '--thres_hold',
-        type=float,
-        default=0.,
-        help='Number of classes')
-    parser.add_argument(
         '--num_classes',
         type=int,
         default=0,
         help='Number of classes')
     parser.add_argument(
-        '--factor',
-        type=int,
-        default=4,
-        help='Scale factor for tail projections')
+        '--thres_hold',
+        type=float,
+        default=0.,
+        help='Number of classes')
 
     argv, _ = parser.parse_known_args()
     tf.get_logger().setLevel(logging.INFO)

examples/adaprive_softmax/text8/model.py
@@ -3,12 +3,11 @@ from __future__ import division
 from __future__ import print_function
 
 import tensorflow as tf
-from tfmiss.keras.layers import AdaptiveSoftmax, NoiseContrastiveEstimation, SampledSofmax
+from tfmiss.keras.layers import AdaptiveSoftmax, SampledSofmax
 
 
 class Text8Model(tf.keras.Model):
     OUT_ASM = 'ASS'
-    OUT_NCE = 'NCE'
     OUT_SS = 'SS'
     OUT_SM = 'SM'
 
@@ -21,8 +20,6 @@ class Text8Model(tf.keras.Model):
 
         if self.OUT_ASM == core:
             decoder = AdaptiveSoftmax(vocab_size, cutoff=cutoff)
-        elif self.OUT_NCE == core:
-            decoder = NoiseContrastiveEstimation(vocab_size, negatives=negatives)
         elif self.OUT_SS == core:
             decoder = SampledSofmax(vocab_size, negatives=negatives)
         else:

examples/adaprive_softmax/text8/text8_test.py
@@ -13,13 +13,13 @@ from utils import data_generator
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Sequence Modeling - Character Level Language Model')
-    parser.add_argument('--batch_size', type=int, default=256, help='Batch size')
+    parser.add_argument('--batch_size', type=int, default=128, help='Batch size')
     parser.add_argument('--dropout', type=float, default=0.1, help='Dropout applied to layers')
-    parser.add_argument('--epochs', type=int, default=100, help='Upper epoch limit')
+    parser.add_argument('--epochs', type=int, default=10, help='Upper epoch limit')
     parser.add_argument('--lr', type=float, default=0.1, help='Initial learning rate')
     parser.add_argument('--embed_size', type=int, default=100, help='Dimension of character embeddings')
     parser.add_argument('--nhid', type=int, default=512, help='Number of hidden units per layer')
-    parser.add_argument('--seq_len', type=int, default=400, help='Total sequence length, including effective history')
+    parser.add_argument('--seq_len', type=int, default=20, help='Total sequence length, including effective history')
     parser.add_argument('--seed', type=int, default=1111, help='Random seed')
     parser.add_argument('--cutoff', type=str, default='2000,10000', help='Adaptive softmax cutoffs')
     parser.add_argument('--negatives', type=int, default=10, help='Number of negative examples for sampled softmaxes')
@@ -34,7 +34,7 @@ if __name__ == "__main__":
 
     train_dataset, test_dataset, vocab_size = data_generator(argv.seq_len, argv.batch_size)
 
-    titles = ['Softmax', 'NoiseContrastiveEstimation', 'SampledSoftmax', 'AdaptiveSoftmax (default)',
+    titles = ['Softmax', 'SampledSoftmax', 'AdaptiveSoftmax (default)',
               'AdaptiveSoftmax (accurate)', 'AdaptiveSoftmax (fast)']
     models = [
         Text8Model(
@@ -43,17 +43,6 @@ if __name__ == "__main__":
             embed_size=argv.embed_size,
             units=argv.nhid,
             core=Text8Model.OUT_SM,
-            dropout=argv.dropout,
-            cutoff=argv_cutoff,
-            negatives=argv.negatives
-        ),
-
-        Text8Model(
-            seq_len=argv.seq_len,
-            vocab_size=vocab_size,
-            embed_size=argv.embed_size,
-            units=argv.nhid,
-            core=Text8Model.OUT_NCE,
             dropout=argv.dropout,
             cutoff=argv_cutoff,
             negatives=argv.negatives
@@ -87,7 +76,7 @@ if __name__ == "__main__":
             units=argv.nhid,
             core=Text8Model.OUT_ASM,
             dropout=argv.dropout,
-            cutoff=[2030, 6774],
+            cutoff=[3526, 13950],
             negatives=argv.negatives
         ),
         Text8Model(
@@ -97,7 +86,7 @@ if __name__ == "__main__":
             units=argv.nhid,
             core=Text8Model.OUT_ASM,
             dropout=argv.dropout,
-            cutoff=[454, 6590],
+            cutoff=[454, 7358],
             negatives=argv.negatives
         ),
     ]
@@ -108,7 +97,7 @@ if __name__ == "__main__":
         model.compile(
             run_eagerly=False,
             optimizer=keras.optimizers.SGD(lr=argv.lr),
-            loss='sparse_categorical_crossentropy',
+            loss=loss,
         )
         model.summary()
 
@@ -119,18 +108,18 @@ if __name__ == "__main__":
             validation_data=test_dataset,
         ).history
         histories[title]['time'] = (time.time() - start_time) / 60
-        histories[title]['ppl'] = np.exp(histories[title]['val_loss'])
+        histories[title]['ce'] = histories[title]['val_loss']
 
     cmap = pyplot.get_cmap('viridis')
     colors = cmap(np.linspace(0, 1, len(histories.items())))
 
     for (core, m), color in zip(histories.items(), colors):
         interval = np.linspace(0, m['time'], argv.epochs)
-        pyplot.plot(interval, m['ppl'], color=color, label=core)
+        pyplot.plot(interval, m['ce'], color=color, label=core)
 
-    pyplot.title('Character LM on {}'.format(argv.dataset.upper()))
+    pyplot.title('Text8 word LM')
     pyplot.xlabel('Time (minutes)')
-    pyplot.ylabel('Perplexity')
+    pyplot.ylabel('Crossentropy')
     pyplot.legend()
-    pyplot.savefig('ppl.png')
+    pyplot.savefig('loss.png')
     pyplot.close()