ff371567a12ffc1e585acc484d26c6cf078f7fc9
Shkarupa Alex <shkarupa.alex@gmail.com>
2020-12-30 11:20:20 +0300

    Attention layers

--- tfmiss/keras/layers/attention.py
+++ tfmiss/keras/layers/attention.py
@@ -8,8 +8,8 @@ from tensorflow.python.keras.utils import tf_utils
 
 
 @utils.register_keras_serializable(package='Miss')
-class AttentionWithContext(layers.Layer):
-    """Attention layer, with a context/query vector, for temporal data.
+class SelfAttentionWithContext(layers.Layer):
+    """Self-attention layer for temporal data.
 
     Reference: https://www.cs.cmu.edu/~diyiy/docs/naacl16.pdf
     Hierarchical Attention Networks for Document Classification
@@ -29,16 +29,17 @@ class AttentionWithContext(layers.Layer):
         self.kernel_constraint = constraints.get(kernel_constraint)
         self.bias_constraint = constraints.get(bias_constraint)
 
-        super(AttentionWithContext, self).__init__(**kwargs)
+        super(SelfAttentionWithContext, self).__init__(**kwargs)
 
     @tf_utils.shape_type_conversion
     def build(self, input_shape):
-        self.channels = input_shape[-1]
-        if self.channels is None:
-            raise ValueError('Channel dimension of predictions should be defined. Found `None`.')
+        channels = input_shape[-1]
+        if channels is None:
+            raise ValueError('Channel dimension of inputs should be defined. Found `None`.')
+        self.input_spec = layers.InputSpec(ndim=3, axes={-1: channels})
 
         self.representation = layers.Dense(
-            self.channels,
+            channels,
             kernel_initializer=self.kernel_initializer,
             bias_initializer=self.bias_initializer,
             kernel_regularizer=self.kernel_regularizer,
@@ -47,18 +48,14 @@ class AttentionWithContext(layers.Layer):
             bias_constraint=self.bias_constraint
         )
         self.importance = layers.Dense(
-            self.channels,
+            channels,
             use_bias=False,
             kernel_initializer=self.kernel_initializer,
             kernel_regularizer=self.kernel_regularizer,
             kernel_constraint=self.kernel_constraint,
         )
 
-        super(AttentionWithContext, self).build(input_shape)
-
-    def compute_mask(self, inputs, mask=None):
-        # do not pass the mask to the next layers
-        return None
+        super(SelfAttentionWithContext, self).build(input_shape)
 
     def call(self, inputs, mask=None):
         uit = self.representation(inputs)
@@ -81,12 +78,16 @@ class AttentionWithContext(layers.Layer):
 
         return outputs
 
+    def compute_mask(self, inputs, mask=None):
+        # do not pass the mask to the next layers
+        return None
+
     @tf_utils.shape_type_conversion
     def compute_output_shape(self, input_shape):
         return input_shape[0], input_shape[-1]
 
     def get_config(self):
-        config = super(AttentionWithContext, self).get_config()
+        config = super(SelfAttentionWithContext, self).get_config()
         config.update({
             'kernel_initializer': initializers.serialize(self.kernel_initializer),
             'bias_initializer': initializers.serialize(self.bias_initializer),
@@ -96,3 +97,128 @@ class AttentionWithContext(layers.Layer):
             'bias_constraint': constraints.serialize(self.bias_constraint)
         })
         return config
+
+
+@utils.register_keras_serializable(package='Miss')
+class MultiplicativeSelfAttention(layers.Attention):
+    """Multiplicative (Luong) self-attention layer for temporal data.
+
+    This version of Self-Attention refers to Transformers
+    """
+
+    def __init__(self, **kwargs):
+        super(MultiplicativeSelfAttention, self).__init__(**kwargs)
+        self.input_spec = layers.InputSpec(ndim=3)
+
+    @tf_utils.shape_type_conversion
+    def build(self, input_shape):
+        channels = input_shape[-1]
+        if channels is None:
+            raise ValueError('Channel dimension of inputs should be defined. Found `None`.')
+        self.input_spec = layers.InputSpec(ndim=3, axes={-1: channels})
+
+        self.make_query = layers.Dense(channels, use_bias=False)
+        self.make_value = layers.Dense(channels, use_bias=False)
+        self.make_key = layers.Dense(channels, use_bias=False)
+
+        super(MultiplicativeSelfAttention, self).build([input_shape, input_shape, input_shape])
+
+    def call(self, inputs, mask=None, training=None, return_attention_scores=False):
+        if training is None:
+            training = tf.keras.backend.learning_phase()
+
+        query = self.make_query(inputs)
+        value = self.make_value(inputs)
+        key = self.make_key(inputs)
+
+        floatx = tf.keras.backend.floatx()  # Hack for wrong cast in _apply_scores
+        tf.keras.backend.set_floatx(self.compute_dtype)
+
+        outputs = super(MultiplicativeSelfAttention, self).call(
+            [query, value, key],
+            mask=None if mask is None else [mask, mask],
+            training=training,
+            return_attention_scores=return_attention_scores
+        )
+
+        tf.keras.backend.set_floatx(floatx)
+
+        return outputs
+
+    def compute_mask(self, inputs, mask=None):
+        return super(MultiplicativeSelfAttention, self).compute_mask(
+            [inputs, inputs, inputs],
+            mask=None if mask is None else [mask, mask])
+
+    @tf_utils.shape_type_conversion
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+
+@utils.register_keras_serializable(package='Miss')
+class AdditiveSelfAttention(layers.AdditiveAttention):
+    """Additive (Bahdanau) self-attention layer for temporal data.
+
+    Reference: https://arxiv.org/pdf/1806.01264.pdf
+    """
+
+    def __init__(self, **kwargs):
+        super(AdditiveSelfAttention, self).__init__(**kwargs)
+        self.input_spec = layers.InputSpec(ndim=3)
+
+    @tf_utils.shape_type_conversion
+    def build(self, input_shape):
+        channels = input_shape[-1]
+        if channels is None:
+            raise ValueError('Channel dimension of inputs should be defined. Found `None`.')
+        self.input_spec = layers.InputSpec(ndim=3, axes={-1: channels})
+
+        self.make_query = layers.Dense(channels, use_bias=False)
+        self.make_key = layers.Dense(channels)
+        self.make_score = layers.Dense(1, activation='sigmoid')
+
+        super(AdditiveSelfAttention, self).build([input_shape, input_shape, input_shape])
+
+    def call(self, inputs, mask=None, training=None, return_attention_scores=False):
+        if training is None:
+            training = tf.keras.backend.learning_phase()
+
+        query = self.make_query(inputs)
+        value = inputs
+        key = self.make_key(inputs)
+
+        floatx = tf.keras.backend.floatx()  # Hack for wrong cast in _apply_scores
+        tf.keras.backend.set_floatx(self.compute_dtype)
+
+        outputs = super(AdditiveSelfAttention, self).call(
+            [query, value, key],
+            mask=None if mask is None else [mask, mask],
+            training=training,
+            return_attention_scores=return_attention_scores
+        )
+
+        tf.keras.backend.set_floatx(floatx)
+
+        return outputs
+
+    def _calculate_scores(self, query, key):
+        q_reshaped = tf.expand_dims(query, axis=-2)
+        k_reshaped = tf.expand_dims(key, axis=-3)
+        if self.use_scale:
+            scale = self.scale
+        else:
+            scale = 1.
+
+        scores = tf.tanh(q_reshaped + k_reshaped)
+        scores = self.make_score(scores)  # This stage missed in parent implementation
+
+        return tf.reduce_sum(scale * scores, axis=-1)
+
+    def compute_mask(self, inputs, mask=None):
+        return super(AdditiveSelfAttention, self).compute_mask(
+            [inputs, inputs, inputs],
+            mask=None if mask is None else [mask, mask])
+
+    @tf_utils.shape_type_conversion
+    def compute_output_shape(self, input_shape):
+        return input_shape
--- tfmiss/keras/layers/attention_test.py
+++ tfmiss/keras/layers/attention_test.py
@@ -4,36 +4,104 @@ from __future__ import print_function
 
 import tensorflow as tf
 from tensorflow.python.keras import keras_parameterized, testing_utils
-from tfmiss.keras.layers.attention import AttentionWithContext
+from tfmiss.keras.layers.attention import SelfAttentionWithContext, MultiplicativeSelfAttention, AdditiveSelfAttention
 
 
 @keras_parameterized.run_all_keras_modes
-class AttentionWithContextTest(keras_parameterized.TestCase):
+class SelfAttentionWithContextTest(keras_parameterized.TestCase):
     def setUp(self):
-        super(AttentionWithContextTest, self).setUp()
+        super(SelfAttentionWithContextTest, self).setUp()
         self.default_policy = tf.keras.mixed_precision.global_policy()
         self.mf16_policy = tf.keras.mixed_precision.Policy('mixed_float16')
 
     def tearDown(self):
-        super(AttentionWithContextTest, self).tearDown()
+        super(SelfAttentionWithContextTest, self).tearDown()
         tf.keras.mixed_precision.set_global_policy(self.default_policy)
 
     def test_layer(self):
         testing_utils.layer_test(
-            AttentionWithContext,
+            SelfAttentionWithContext,
             kwargs={},
             input_shape=(2, 10, 5),
             input_dtype='float32',
-            expected_output_dtype='float32'
+            expected_output_dtype='float32',
+            expected_output_shape=(None, 5)
         )
 
         tf.keras.mixed_precision.set_global_policy(self.mf16_policy)
         testing_utils.layer_test(
-            AttentionWithContext,
+            SelfAttentionWithContext,
             kwargs={},
             input_shape=(2, 10, 5),
             input_dtype='float16',
-            expected_output_dtype='float16'
+            expected_output_dtype='float16',
+            expected_output_shape=(None, 5)
+        )
+        tf.keras.mixed_precision.set_global_policy(self.default_policy)
+
+
+@keras_parameterized.run_all_keras_modes
+class MultiplicativeSelfAttentionTest(keras_parameterized.TestCase):
+    def setUp(self):
+        super(MultiplicativeSelfAttentionTest, self).setUp()
+        self.default_policy = tf.keras.mixed_precision.global_policy()
+        self.mf16_policy = tf.keras.mixed_precision.Policy('mixed_float16')
+
+    def tearDown(self):
+        super(MultiplicativeSelfAttentionTest, self).tearDown()
+        tf.keras.mixed_precision.set_global_policy(self.default_policy)
+
+    def test_layer(self):
+        testing_utils.layer_test(
+            MultiplicativeSelfAttention,
+            kwargs={},
+            input_shape=(2, 10, 5),
+            input_dtype='float32',
+            expected_output_dtype='float32',
+            expected_output_shape=(None, 10, 5)
+        )
+
+        tf.keras.mixed_precision.set_global_policy(self.mf16_policy)
+        testing_utils.layer_test(
+            MultiplicativeSelfAttention,
+            kwargs={'use_scale': True, 'causal': True, 'dropout': 0.1},
+            input_shape=(2, 10, 5),
+            input_dtype='float16',
+            expected_output_dtype='float16',
+            expected_output_shape=(None, 10, 5)
+        )
+        tf.keras.mixed_precision.set_global_policy(self.default_policy)
+
+
+@keras_parameterized.run_all_keras_modes
+class AdditiveSelfAttentionTest(keras_parameterized.TestCase):
+    def setUp(self):
+        super(AdditiveSelfAttentionTest, self).setUp()
+        self.default_policy = tf.keras.mixed_precision.global_policy()
+        self.mf16_policy = tf.keras.mixed_precision.Policy('mixed_float16')
+
+    def tearDown(self):
+        super(AdditiveSelfAttentionTest, self).tearDown()
+        tf.keras.mixed_precision.set_global_policy(self.default_policy)
+
+    def test_layer(self):
+        testing_utils.layer_test(
+            AdditiveSelfAttention,
+            kwargs={},
+            input_shape=(2, 10, 5),
+            input_dtype='float32',
+            expected_output_dtype='float32',
+            expected_output_shape=(None, 10, 5)
+        )
+
+        tf.keras.mixed_precision.set_global_policy(self.mf16_policy)
+        testing_utils.layer_test(
+            AdditiveSelfAttention,
+            kwargs={'use_scale': True, 'causal': True, 'dropout': 0.1},
+            input_shape=(2, 10, 5),
+            input_dtype='float16',
+            expected_output_dtype='float16',
+            expected_output_shape=(None, 10, 5)
         )
         tf.keras.mixed_precision.set_global_policy(self.default_policy)
 
