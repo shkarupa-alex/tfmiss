640aecd5aa140d46d9c1abfa7838088658ff0308
Shkarupa Alex <shkarupa.alex@gmail.com>
2021-06-07 14:04:24 +0300

    Update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 from setuptools.command.install import install
 from setuptools.dist import Distribution
 
-__VERSION__ = '0.12.0'
+__VERSION__ = '0.12.1'
 
 
 class InstallPlatlib(install):
