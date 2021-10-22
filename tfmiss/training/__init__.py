8a140697587562608d307b3200e1df5c60660f4c
Shkarupa Alex <shkarupa.alex@gmail.com>
2021-10-22 22:01:05 +0300

    Remove hparam

--- tfmiss/training/__init__.py
+++ tfmiss/training/__init__.py
@@ -4,4 +4,3 @@ from __future__ import print_function
 
 from tfmiss.training.adapt import test_device_matmul, build_zipf_vocab, estimate_best_splits
 from tfmiss.training.bucket import estimate_bucket_boundaries, estimate_bucket_pipeline
-from tfmiss.training.hparam import HParams
