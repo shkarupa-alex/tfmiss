4ed5306576c8a8e52cd64c34d5484b004359361a
Shkarupa Alex <shkarupa.alex@gmail.com>
2022-06-24 14:43:46 +0300

    Update version, remove old test

--- tfmiss/keras/layers/scale_test.py
+++ tfmiss/keras/layers/scale_test.py
@@ -27,35 +27,6 @@ class L2ScaleTest(test_combinations.TestCase):
             expected_output_dtype='float16'
         )
 
-    def test_ragged_input(self):
-        layer = L2Scale(alpha=10)
-        logits = tf.ragged.constant([
-            [[1., 2.], [2., 3.], [2., 5.]],
-            [[0., 9.]],
-            [[1., 1.], [2., 9.]]
-        ], ragged_rank=1)
-        inputs = layers.Input(shape=(None, 2), dtype=tf.float32, ragged=True)
-        outputs = layer(inputs)
-
-        model = models.Model(inputs=inputs, outputs=outputs)
-        model.run_eagerly = test_utils.should_run_eagerly()
-        predictions = model.predict(logits)
-        self.assertAllClose(
-            predictions,
-            tf.ragged.constant([
-                [
-                    [4.4721360206604, 8.9442720413208],
-                    [5.547001838684082, 8.320503234863281],
-                    [3.713906764984131, 9.284767150878906]
-                ],
-                [[0.0, 10.0]],
-                [
-                    [7.071067810058594, 7.071067810058594],
-                    [2.169304609298706, 9.761870384216309]
-                ]
-            ], ragged_rank=1)
-        )
-
     def test_with_ragged(self):
         layer = WithRagged(L2Scale(alpha=10))
         logits = tf.ragged.constant([
