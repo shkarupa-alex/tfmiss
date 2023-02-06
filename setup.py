46fa62186ac2723e974710387196488adf2d5f5f
Shkarupa Alex <shkarupa.alex@gmail.com>
2023-02-06 17:26:16 +0300

    Update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from setuptools import setup, find_packages, Extension
 from setuptools.dist import Distribution
 
-__VERSION__ = '0.19.0'
+__VERSION__ = '0.19.1'
 
 
 def get_ext_modules():
