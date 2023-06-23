f211a3620bd980ca68078507b72c004ba56fffe5
Shkarupa Alex <shkarupa.alex@gmail.com>
2023-06-23 17:00:54 +0300

    Drop Lion optimizer (now in keras)

--- tfmiss/keras/optimizers/__init__.py
+++ tfmiss/keras/optimizers/__init__.py
@@ -5,4 +5,3 @@ from __future__ import print_function
 from tfmiss.keras.optimizers import schedules
 from tfmiss.keras.optimizers.accum import Accum
 from tfmiss.keras.optimizers.adan import Adan
-from tfmiss.keras.optimizers.lion import Lion
